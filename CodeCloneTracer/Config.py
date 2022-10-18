3d700b437ef55762510a0c8620f1d1d37677229a
SoujanyaBasangari <65566187+SoujanyaBasangari@users.noreply.github.com>
2022-10-19 01:35:30 +0530

    Update Config.py

--- CodeCloneTracer/Config.py
+++ CodeCloneTracer/Config.py
@@ -1,9 +1,9 @@
 
 dirPath = "D:/projects/clone/test_project/"
 
-url = "https://github.com/shashirajraja/onlinebookstore"
+url = "https://github.com/SoujanyaBasangari/test_project.git"
 
-extract_from_git = False
+extract_from_git = True
 git_first=False
 # Minimum length of block to consider
 minimumLengthBlock = 10
